32865fae9fa10180299ea079eb10ca502e47696b
Wycer <wycers@gmail.com>
2020-02-22 03:57:29 +0800

    apply vuetify

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import App from './App';
 import store from '../store';
-import router from './router';
+import router from '../router';
+import vuetify from '../plugins/vuetify';
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   store,
+  vuetify,
   router,
   render: h => h(App),
 });
