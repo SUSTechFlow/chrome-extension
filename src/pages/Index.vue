152fed55e3f7131de043762600959518c7f7bf76
Wycer <wycers@gmail.com>
2020-02-23 14:50:18 +0800

    layout change

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,21 +1,7 @@
 <template>
-  <v-app>
-    <v-app-bar app color="white" height="45px">
-      <v-toolbar-title class="ml-0 pl-1 mr-1">
-        <router-link :to="{ name: 'home' }" class="google-font" style="text-decoration:none; color: rgba(0,0,0,.87);">
-          SUSTech Flow
-        </router-link>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn @click="navigateToGithub" class="ml-0 google-font" style="text-transform: capitalize;" text>
-        Github
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <v-container> <Courses></Courses></v-container>
-    </v-content>
-  </v-app>
+  <v-container>
+    <Courses></Courses>
+  </v-container>
 </template>
 <script>
 import Courses from '@/components/Courses';
